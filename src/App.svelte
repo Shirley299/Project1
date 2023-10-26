d47e9ddabf30da2da9a8f9983a560acca0531d75
Gabriel Zhong <mud.miscue_0l@icloud.com>
2023-10-27 00:11:29 +0900

    fix nav

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import { Router, Link, Route } from 'svelte-routing'
+  import { onMount } from 'svelte'
   import Home from './lib/Home.svelte'
   import Catalog from './lib/Catalog.svelte'
   import User from './lib/User.svelte'
@@ -15,6 +16,22 @@
   const click = (index) => {
     classList = classList.map((item, idx) => idx === index ? 'list active' : 'list')
   }
+  onMount(() => {
+    let pathName = window.location.pathname.split('/')
+    pathName = pathName.filter(item => item !== '')
+    if (pathName.length === 1 && pathName[0] === 'Project1') {
+      classList = classList.map((item, idx) => idx === 0 ? 'list active' : 'list')
+    }
+    else if (pathName.length === 2 && pathName[1] === 'catalog') {
+      classList = classList.map((item, idx) => idx === 1 ? 'list active' : 'list')
+    }
+    else if (pathName.length === 2 && pathName[1] === 'user') {
+      classList = classList.map((item, idx) => idx === 2 ? 'list active' : 'list')
+    }
+    else if (pathName.length === 2 && pathName[1] === 'about') {
+      classList = classList.map((item, idx) => idx === 3 ? 'list active' : 'list')
+    }
+  })
 </script>
 
 <main>
